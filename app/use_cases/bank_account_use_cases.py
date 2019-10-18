472de8082d773ecea0afc6c918e120668534fbde
Hiruma <yapadubpadoo@gmail.com>
2019-10-18 12:03:16 +0700

    Implement Saving Accoun use case and test

---
+++ app/use_cases/bank_account_use_cases.py
@@ -0,0 +1,12 @@
+class BankAccountUseCases:
+    def __init__(self, bank_account_repo, saving_interest_calculator):
+        self.bank_account_repo = bank_account_repo
+        self.saving_interest_calculator = saving_interest_calculator
+
+    def update_saving_account_with_saving_interest(self, account_id):
+        saving_account = self.bank_account_repo.get_by_account_id(account_id)
+        current_balance = saving_account.get_balance()
+        interest = self.saving_interest_calculator.get_interest_from_balance(
+            current_balance)
+        saving_account.set_balance(current_balance+interest)
+        self.bank_account_repo.update(saving_account)
---
+++ tests/use_cases/test_saving_account_use_cases.py
@@ -0,0 +1,38 @@
+import unittest
+from unittest.mock import MagicMock
+from app.use_cases.bank_account_use_cases import BankAccountUseCases
+
+
+class TestBankAccountUseCases(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_update_saving_account_with_saving_interest(self):
+        saving_account = MagicMock()
+        saving_account.get_balance = MagicMock(return_value=1000)
+        saving_account.set_balance = MagicMock()
+
+        bank_account_repo = MagicMock()
+        bank_account_repo.get_by_account_id = MagicMock(
+            return_value=saving_account)
+        bank_account_repo.update = MagicMock()
+
+        saving_interest_calculator = MagicMock()
+        saving_interest_calculator.get_interest_from_balance = MagicMock(
+            return_value=10)
+
+        bank_account_use_cases = BankAccountUseCases(
+            bank_account_repo, saving_interest_calculator)
+
+        bank_account_use_cases.update_saving_account_with_saving_interest(
+            account_id='103-117-446')
+
+        bank_account_repo.get_by_account_id.assert_called_once_with(
+            '103-117-446')
+
+        saving_account.get_balance.assert_called_once()
+        saving_interest_calculator.get_interest_from_balance.assert_called_once_with(
+            1000)
+        saving_account.set_balance.assert_called_once_with(1010)
+
+        bank_account_repo.update.assert_called_once_with(saving_account)
